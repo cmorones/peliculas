193e34f77ff7451c3042cc6a82355fb213d173d3
Cesar Morones <cesar.morones1@gmail>
2022-05-14 13:52:10 -0500

    Entrega

---
+++ app/src/main/java/com/app/cmorones/peliculas/model/Movie.kt
@@ -0,0 +1,9 @@
+package com.app.cmorones.peliculas.model
+
+data class Movie(
+    var id: Int,
+    var title: String,
+    var description: String,
+    var genre: String,
+    var date: String
+)
